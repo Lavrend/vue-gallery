3c5b680c5d31a10eb00c1afaf55f665486394b6c
Lavrentev Dmitriy <lavrend7@gmail.com>
2019-02-16 20:46:30 +0500

    fix homepage (add header, add bg, add description)

--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -1,13 +1,26 @@
 <template lang="pug">
   .layout-home
+    header.layout-home__header
+      .layout-home__header-wrapper
+        Logo.layout-home__logo
+        LangsBlock.layout-home__langs
+
     main.layout-home__content
       transition(name="transition-zoom" mode="out-in")
         slot
 </template>
 
 <script>
+import Logo from '@/components/Logo';
+import LangsBlock from '@/components/Langs';
+
 export default {
   name: 'layout-home',
+
+  components: {
+    Logo,
+    LangsBlock,
+  },
 };
 </script>
 
@@ -21,6 +34,43 @@ export default {
 
   display: flex;
   flex-flow: column nowrap;
+
+  &__header {
+    position: absolute;
+    width: 100%;
+    height: $headerHeight;
+    min-width: $screenMinWidth;
+    top: 0;
+    left: 0;
+
+    user-select: none;
+
+    // Need for display the layer correct (above content layer)
+    z-index: $zIndex-200;
+  }
+
+  &__header-wrapper {
+    width: 100%;
+    height: 100%;
+    max-width: $screenMaxWidth;
+    margin: 0 auto;
+    padding: 0 $indent-lg;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__logo {
+    height: 100%;
+    font-size: 18px;
+    flex: 0;
+  }
+
+  &__langs {
+    height: 100%;
+    flex: 0;
+  }
 
   &__content {
     width: 100%;
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,24 @@
 <template lang="pug">
   section.page-home
-    .page-home__content
-      h1.page-about__title
-        | Home page
+    .page-home__background
+      .page-home__bg-picture
+      .page-home__bg-overlay
 
-      .page-home__btn.page-home__btn--about(@click="$router.push({ name: 'about' })")
-        | About
+    .page-home__content
+      .page-home__flex
+        .page-home__flex-title(v-html="$t('HOMEPAGE.title')")
+        .page-home__flex-description(v-html="$t('HOMEPAGE.description')")
+
+        .page-home__flex-buttons
+          .page-home__btn.page-home__btn--gallery(
+            v-html="$t('HOMEPAGE.btn_gallery')"
+            @click="$router.push({ name: 'gallery' })"
+          )
+
+          .page-home__btn.page-home__btn--about(
+            v-html="$t('HOMEPAGE.btn_about')"
+            @click="$router.push({ name: 'about' })"
+          )
 </template>
 
 <script>
@@ -21,6 +34,35 @@ export default {
   min-height: $screenMinWidth;
   text-align: center;
 
+  &__background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+  }
+
+  &__bg-picture {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+
+    background: url('~@/assets/images/bg_homepage.jpeg'), url($bgHomepageBase64);
+    background-size: cover;
+    background-position: center;
+  }
+
+  &__bg-overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background: rgba($black, 0.7);
+  }
+
   &__content {
     position: absolute;
     width: 100%;
@@ -30,6 +72,31 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
+  }
+
+  &__flex {
+    margin-top: $indent-lg;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    user-select: none;
+  }
+
+  &__flex-title {
+    font-size: 60px;
+  }
+
+  &__flex-description {
+    margin-top: $indent-md;
+    font-size: 26px;
+    font-style: italic;
+  }
+
+  &__flex-buttons {
+    margin-top: $indent-lg;
+    font-size: 18px;
   }
 
   &__btn {
@@ -44,6 +111,60 @@ export default {
 
     &:hover {
       background: rgba($green-2, 0.7);
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    &__flex-buttons {
+      font-size: 16px;
+    }
+
+    &__btn {
+      width: 160px;
+    }
+  }
+
+  @media screen and (min-width: 681px) and (max-width: 800px) {
+    &__flex-title {
+      font-size: 50px;
+    }
+
+    &__flex-description {
+      font-size: 20px;
+    }
+  }
+
+  @media screen and (min-width: 541px) and (max-width: 680px) {
+    &__flex-title {
+      font-size: 40px;
+    }
+
+    &__flex-description {
+      font-size: 18px;
+    }
+  }
+
+  @media screen and (min-width: 401px) and (max-width: 540px) {
+    &__flex-title {
+      font-size: 30px;
+    }
+
+    &__flex-description {
+      font-size: 16px;
+    }
+  }
+
+  @media screen and (max-width: 400px) {
+    &__flex-title {
+      font-size: 25px;
+    }
+
+    &__flex-description {
+      font-size: 16px;
+    }
+
+    &__btn {
+      width: 120px;
     }
   }
 }
